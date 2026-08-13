1bfd016fe537208544c5714f8ad69decda180918
Babatunde Rahman <babatunderhmn@gmail.com>
2026-08-13 14:36:40 +0000

    updated codes

--- models/Intermidiate/dim_order_intermidiate.sql
+++ models/Intermidiate/dim_order_intermidiate.sql
@@ -1,9 +1,21 @@
-select 
-    *,
-    order_delivered_date,
-    order_estimated_date,
-    date_diff(order_estimated_date,order_delivered_date,day) as date_difference
+with order_table_data_difference as (
+    select *,date_diff(order_estimated_date,order_delivered_date,day) as date_difference
+    from {{ ref('stg_olist_orders_dataset__orders') }}
+),
+date_difference_logic as (
+    select 
+        *, 
+        case 
+            when date_difference = 0 then 'standard_delivery'
+            when date_difference > 0 then 'late_delivery'
+            when date_difference < 0 then 'early_delivery'
+        end as delivery_type
+    from order_table_data_difference
+)
+select *
+from date_difference_logic
 
 
-from {{ ref('stg_olist_orders_dataset__orders') }}
-order by 3 desc 
+
+
+
